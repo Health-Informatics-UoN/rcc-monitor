82318e77519dbed4f957b3ef6722ffaf5ae5263d
Andy Rae <hello@rae.li>
2023-09-14 16:30:43 +0100

    Restore file

---
+++ app/Monitor/Auth/CustomClaimTypes.cs
@@ -0,0 +1,9 @@
+namespace Monitor.Auth;
+
+public static class CustomClaimTypes
+{
+  public const string FullName = "FullName";
+  public const string UICulture = "UICulture";
+  public const string SitePermission = "SitePermission";
+
+}
