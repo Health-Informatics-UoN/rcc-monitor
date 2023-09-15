d4bd4297fbe3f6705908c0b41e4bbf9f7687cacb
Andy Rae <hello@rae.li>
2023-09-15 11:10:50 +0100

    Revert main client-app

--- app/client-app/auth/components/auth-provider.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { SessionProvider, SessionProviderProps } from "next-auth/react";
-
-export function AuthProvider({ children, ...props }: SessionProviderProps) {
-	return <SessionProvider {...props}>{children}</SessionProvider>;
-}
