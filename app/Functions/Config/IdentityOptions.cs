530d30fe8cfb4c577cb57b58a5300aa28d7ec48f
Andy Rae <hello@rae.li>
2023-09-21 14:05:47 +0100

    Rename options

---
+++ app/Functions/Config/IdentityOptions.cs
@@ -0,0 +1,8 @@
+namespace Functions.Config;
+
+public class IdentityOptions
+{
+    public string Address { get; set; }
+    public string ClientId { get; set; }
+    public string Secret { get; set; }
+}
