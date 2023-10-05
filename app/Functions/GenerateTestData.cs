f2bf66ccd6a3a02c7a61bbb0f1a3d1d213f8fbdf
Andy Rae <hello@rae.li>
2023-10-05 11:00:17 +0100

    Add crf name complete header

--- app/Functions/GenerateTestData.cs
+++ app/Functions/GenerateTestData.cs
@@ -24,17 +24,30 @@ public static class GenerateTestData
         };
         import.Cells["A1"].LoadFromText(file, format);
         
-        // Extract the first column data from the 3rd row onwards
-        var columnData = import.Cells[3, 1, import.Dimension.End.Row, 1]
-            .Select(cell => cell.Text)
-            .ToList();
-        
         // Generate header row
         var headerRows = new List<string>();
+        
+        // Initialize variables to keep track of the current and previous form names
+        var currentFormName = string.Empty;
+        var previousFormName = string.Empty;
         
         // Iterate through rows (start from the third row)
         for (var rowIndex = 3; rowIndex <= import.Dimension.End.Row; rowIndex++)
         {
+            // CRF Complete Guard 
+            currentFormName = import.Cells[rowIndex, 2].Text; // Column 2 contains the Form Name
+            if (currentFormName != previousFormName)
+            {
+                if (!string.IsNullOrEmpty(previousFormName))
+                {
+                    // Add a new header column for the completion status of the previous form
+                    headerRows.Add(previousFormName + "_complete");
+                    headerRows.Add(previousFormName + "_custom_label");
+                }
+            }
+            previousFormName = currentFormName;
+
+            // Checkbox guard
             var cellF = import.Cells[rowIndex, 6].Text; // Column F
             var cellH = import.Cells[rowIndex, 8].Text; // Column H
 
@@ -44,14 +57,10 @@ public static class GenerateTestData
                 var choices = cellH.Split('|');
 
                 // Generate header rows based on choices
-                // for (var i = 0; i < choices.Length; i++)
-                // {
-                //     var header = import.Cells[rowIndex, 1].Text + "___" + i;
-                //     headerRows.Add(header);
-                // }
                 foreach (var choice in choices)
                 {
-                    var header = import.Cells[rowIndex, 1].Text + "___" + choice.Trim();
+                    var label = choice.Split(',')[0].Trim('"');
+                    var header = import.Cells[rowIndex, 1].Text + "___" + label.Trim();
                     headerRows.Add(header);
                 }
             }
@@ -60,6 +69,13 @@ public static class GenerateTestData
                 var header = import.Cells[rowIndex, 1].Text;
                 headerRows.Add(header);
             }
+        }
+        
+        // CRF Complete - last form guard?
+        if (!string.IsNullOrEmpty(currentFormName))
+        {
+            headerRows.Add(currentFormName + "_complete");
+            headerRows.Add(previousFormName + "_custom_label");
         }
         
         // Write the header rows to the new sheet
