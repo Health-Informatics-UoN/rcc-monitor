04ff63c0ab60086425162661402290237e794943
Andy Rae <hello@rae.li>
2023-10-05 16:04:11 +0100

    Add date generation

--- app/Functions/Services/SyntheticData/DataGenerator.cs
+++ app/Functions/Services/SyntheticData/DataGenerator.cs
@@ -4,20 +4,32 @@ namespace Functions.Services.SyntheticData;
 
 public abstract class DataGenerator
 {
-    public abstract string GenerateData(string min, string max);
+    public abstract string GenerateData(string? min, string? max);
 }
 
 public class DateBoxGenerator : DataGenerator
 {
-    public override string GenerateData(string min, string max)
+    public override string GenerateData(string? min, string? max)
     {
-        return "Generated Date";
+        // Default min date to a week ago
+        var minDate = !string.IsNullOrEmpty(min) ? DateTime.Parse(min) : DateTime.Now.AddDays(-7);
+
+        // Default max date to today
+        var maxDate = !string.IsNullOrEmpty(max) ? DateTime.Parse(max) : DateTime.Now;
+
+        // Generate a random date between minDate and maxDate
+        var random = new Random();
+        var range = (maxDate - minDate).Days;
+        var generatedDate = minDate.AddDays(random.Next(range));
+        
+        // TODO: Check the default redcap import date format.
+        return generatedDate.ToString("yyyy-MM-dd");
     }
 }
 
 public class TextGenerator : DataGenerator
 {
-    public override string GenerateData(string min, string max)
+    public override string GenerateData(string? min, string? max)
     {
         return "Generated Text";
     }
@@ -25,7 +37,7 @@ public class TextGenerator : DataGenerator
 
 public class NumberGenerator : DataGenerator
 {
-    public override string GenerateData(string min, string max)
+    public override string GenerateData(string? min, string? max)
     {
         return "Generated Text";
     }
