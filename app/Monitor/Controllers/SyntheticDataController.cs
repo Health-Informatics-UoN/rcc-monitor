df513d18ab3dd757277ce48dfc55f6b04eb822e8
Andy Rae <hello@rae.li>
2023-10-13 13:29:29 +0100

    Update backend

--- app/Monitor/Controllers/SyntheticDataController.cs
+++ app/Monitor/Controllers/SyntheticDataController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.FeatureManagement.Mvc;
+using Monitor.Auth;
 using Monitor.Constants;
 using Monitor.Services.SyntheticData;
 
@@ -18,11 +19,14 @@ public class SyntheticDataController : ControllerBase
     {
         _syntheticData = syntheticDataService;
     }
-    
-    [HttpPost("synthetic-data")]
-    public Task<ActionResult<IFormFile>> GenerateSyntheticData(IFormFile file)
+
+    [Authorize(nameof(AuthPolicies.CanGenerateSyntheticData))]
+    [HttpPost("generate")]
+    public Task<ActionResult<IFormFile>> Generate([FromForm] IFormFile file)
     {
+        // TODO: Add server side validation.
         var generatedCsv = _syntheticData.Generate(file);
         return Task.FromResult<ActionResult<IFormFile>>(File(generatedCsv, "text/csv", "generated-data.csv"));
     }
+    
 }
