b50272e10e5ab957fd4867ef962df036b65b5e29
Andy Rae <hello@rae.li>
2023-10-17 10:32:08 +0100

    Add layout continer

--- app/client-app/app/layout.tsx
+++ app/client-app/app/layout.tsx
@@ -1,6 +1,8 @@
+import { Container } from "@/styled-system/jsx";
+import "@/styles/globals.css";
+
 import Navbar from "@/components/Navbar";
 import { Toaster } from "@/components/ui/toast/toaster";
-import "@/styles/globals.css";
 
 export default function RootLayout({
   children,
@@ -11,7 +13,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body>
         <Navbar />
-        {children}
+        <Container>{children}</Container>
         <Toaster />
       </body>
     </html>
