b69c1ee7ffdb848963e1ef3236ede5c9e72edde4
Andy Rae <hello@rae.li>
2023-10-18 09:10:46 +0100

    Add get spreadsheet fetch

--- app/client-app/lib/api/syntheticdata.ts
+++ app/client-app/lib/api/syntheticdata.ts
@@ -4,18 +4,33 @@ import request from "./request";
 
 const fetchKeys = {
   generate: "syntheticdata/generate",
+  get: (filename: string) => `syntheticdata/file?name=${filename}`,
 };
 
 export async function postSpreadsheet(prevState: any, formData: FormData) {
   try {
-    const response: { url: string } = await request(fetchKeys.generate, {
+    const response: { name: string } = await request(fetchKeys.generate, {
       method: "POST",
       body: formData,
     });
 
-    return { message: "success", url: response.url };
+    return { message: "success", name: response.name };
   } catch (error) {
     console.error(error);
     return { message: "Failed to upload file." };
+  }
+}
+
+export async function getSpreadsheet(filename: string) {
+  try {
+    const response = await request(fetchKeys.get(filename), {
+      method: "GET",
+      download: true,
+    });
+
+    return response;
+  } catch (error) {
+    console.error(error);
+    return { message: "Failed to get file." };
   }
 }
