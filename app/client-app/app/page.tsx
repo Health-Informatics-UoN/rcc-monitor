9c2f9bb7ab5d6e389aebb1462da72a41fae7ee05
Andy Rae <hello@rae.li>
2023-10-24 13:31:12 +0100

    Update rules

--- app/client-app/app/page.tsx
+++ app/client-app/app/page.tsx
@@ -1,14 +1,15 @@
-import { css } from "@/styled-system/css";
+import { css } from '@/styled-system/css';
 
 export default function Home() {
+  const x = '';
   return (
     <>
       <h2
         className={css({
-          m: "10px",
-          p: "10px",
-          fontSize: "30px",
-          fontWeight: "500"
+          m: '10px',
+          p: '10px',
+          fontSize: '30px',
+          fontWeight: '500',
         })}
       >
         Welcome.
