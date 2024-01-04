13660672d59970c3a3a8fb77819993984ed5dac8
Andy Rae <hello@rae.li>
2024-01-04 14:57:01 +0000

    Initial working encrypt/decrypt

--- lib/Data/ApplicationDbContext.cs
+++ lib/Data/ApplicationDbContext.cs
@@ -1,13 +1,32 @@
-﻿using Monitor.Data.Entities;
+﻿using System.Security.Cryptography;
+using System.Text;
+using Monitor.Data.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.DataEncryption;
+using Microsoft.EntityFrameworkCore.DataEncryption.Providers;
 
 namespace Monitor.Data;
 
 public class ApplicationDbContext : DbContext
 
 {
+    private readonly byte[] _encryptionKey = Encoding.Unicode.GetBytes("Fk0bI2pC"); // TODO: Read this from config
+    private readonly IEncryptionProvider _provider;
+    
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-        : base(options) { }
+        : base(options)
+    {
+        // TODO: We should be generating a different IV everytime we encrypt.
+        // Ideally the provider would be adding this to the encrypted text
+        // But I don't think it is, so we probably need to use a pair for now.
+        // Or write our own wrapper.
+        var aes = Aes.Create();
+        aes.KeySize = 256;
+        aes.BlockSize = 128;
+        aes.GenerateIV();
+
+        _provider = new AesProvider(Encoding.Unicode.GetBytes("Fk0bI2pCVdppFhEj"), Encoding.Unicode.GetBytes("Fk0bI2pC"));
+    }
     
     public DbSet<Report> Reports => Set<Report>();
     public DbSet<ReportType> ReportTypes => Set<ReportType>();
@@ -21,6 +40,7 @@ public class ApplicationDbContext : DbContext
     protected override void OnModelCreating(ModelBuilder builder)
     {
         base.OnModelCreating(builder);
+        builder.UseEncryption(_provider);
         
         builder.Entity<StudyUser>()
             .HasKey(su => new { su.StudyId, su.UserId });
