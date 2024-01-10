b1ac29a5fe8c8804d4bc76e84cb7aaca1845a1dd
Andy Rae <hello@rae.li>
2024-01-10 12:23:26 +0000

    Add migrations

---
+++ lib/Data/Migrations/20240110122201_ProductionSubjectsEnteredAlert.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Monitor.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductionSubjectsEnteredAlert : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "ProductionSubjectsEnteredAlert",
+                table: "Studies",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductionSubjectsEnteredAlert",
+                table: "Studies");
+        }
+    }
+}
