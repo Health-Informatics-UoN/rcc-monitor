5aeec039cff6cc2f92ef164a5499947626d84f28
Andy Rae <hello@rae.li>
2024-01-31 12:42:46 +0000

    Add refreshAccessToken function

--- app/frontend-webapp/auth/options.ts
+++ app/frontend-webapp/auth/options.ts
@@ -3,6 +3,47 @@ import { JWT } from "next-auth/jwt";
 import { Provider } from "next-auth/providers/index";
 import KeycloakProvider from "next-auth/providers/keycloak";
 
+async function refreshAccessToken(token: JWT) {
+  try {
+    const url = process.env.KEYCLOAK_ISSUER + "/protocol/openid-connect/token";
+
+    const params = new URLSearchParams({
+      client_id: process.env.KEYCLOAK_ID || "",
+      client_secret: process.env.KEYCLOAK_SECRET || "",
+      grant_type: "refresh_token",
+      refresh_token: token.refresh_token,
+    });
+
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      method: "POST",
+      body: params,
+    });
+
+    const refreshedTokens = await response.json();
+
+    if (!response.ok) {
+      throw refreshedTokens;
+    }
+
+    return {
+      ...token,
+      access_token: refreshedTokens.access_token,
+      expires_at: Date.now() / 1000 + refreshedTokens.expires_in,
+      refresh_token: refreshedTokens.refresh_token ?? token.refreshToken, // Fall back to old refresh token
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    };
+  }
+}
+
 export const options: NextAuthOptions = {
   providers: [
     KeycloakProvider({
@@ -29,11 +70,11 @@ export const options: NextAuthOptions = {
         });
       }
 
-      // token has not expired yet so return it.
+      // Token has not expired yet so return it.
       if (now < token.expires_at) return token;
 
-      // TODO: Token has expired to update it
-      return token;
+      // Token has expired so update and return it.
+      return refreshAccessToken(token);
     },
 
     async session({
