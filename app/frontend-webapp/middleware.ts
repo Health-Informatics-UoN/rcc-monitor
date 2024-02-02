c47a295c9b8204edd2b439e8753c899045739326
Andy Rae <hello@rae.li>
2024-02-02 10:00:35 +0000

    Add a plausible policy path mapping

--- app/frontend-webapp/middleware.ts
+++ app/frontend-webapp/middleware.ts
@@ -3,6 +3,7 @@ import { JWT } from "next-auth/jwt";
 import { withAuth } from "next-auth/middleware";
 import { pathToRegexp } from "path-to-regexp";
 
+import { AuthorizationPolicies } from "@/auth/AuthPolicies";
 import { permissions } from "@/auth/permissions";
 
 // Map the permission that a path needs to be accessed.
@@ -14,6 +15,28 @@ const permissionPathMapping = {
   [permissions.EditConfig]: ["/settings"],
 };
 
+// List of policies to check
+// TODO: probably move this into the auth section somewhere
+// as we're effectively "registering" policies here.
+const policiesToCheck = [
+  AuthorizationPolicies.CanViewSiteReports,
+  AuthorizationPolicies.CanGenerateSyntheticData,
+  AuthorizationPolicies.CanViewStudies,
+  AuthorizationPolicies.CanDeleteStudies,
+  AuthorizationPolicies.CanRemoveStudyUsers,
+  AuthorizationPolicies.CanViewUsers,
+  AuthorizationPolicies.CanEditConfig,
+];
+
+const policyPathMapping = {
+  "/reports": AuthorizationPolicies.CanViewSiteReports,
+  "/reports/resolved": AuthorizationPolicies.CanViewSiteReports,
+  "/synthetic-data": AuthorizationPolicies.CanGenerateSyntheticData,
+  "/studies": AuthorizationPolicies.CanViewStudies,
+  "/studies:id/": AuthorizationPolicies.CanViewStudies,
+  "/settings": AuthorizationPolicies.CanEditConfig,
+};
+
 export default withAuth({
   callbacks: {
     authorized: ({ req, token }: { req: NextRequest; token: JWT | null }) => {
@@ -21,6 +44,12 @@ export default withAuth({
       const userPermissions: any = token?.permissions;
 
       const currentPath = req.nextUrl.pathname;
+
+      // need to filter the policies to check based on the paths... ?
+
+      const isAuthorized = policiesToCheck.some((policy) =>
+        policy.isAuthorized(userPermissions)
+      );
 
       for (const permission in permissionPathMapping) {
         for (const mappedPath of permissionPathMapping[permission]) {
