c80df6e9c159607c1e392ca85ac2717891527db1
Andy Rae <hello@rae.li>
2024-02-05 12:53:39 +0000

    Add todo

--- app/frontend-webapp/lib/auth/isUserAuthorized.ts
+++ app/frontend-webapp/lib/auth/isUserAuthorized.ts
@@ -1,5 +1,6 @@
 /**
- * Checks a list of permission contains a specific permission.
+ * TODO: Make this token / policy based.
+ * Checks if a token meets a specific authorization policy.
  * @param permissions list of permissions to check
  * @param permissionToCheck permission to check
  * @returns true if the permission exists in the list
