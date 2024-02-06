df221552af95f6123e0168262edd2ef0b82acd48
Andy Rae <hello@rae.li>
2024-02-06 10:41:49 +0000

    Remove default config middleware

--- app/frontend-webapp/middleware.ts
+++ app/frontend-webapp/middleware.ts
@@ -3,7 +3,6 @@ import { JWT } from "next-auth/jwt";
 import { withAuth } from "next-auth/middleware";
 
 import { policyPathMapping } from "@/auth/PolicyPathMapping";
-import { defaultConfig } from "@/lib/middlewares/defaultConfig";
 import { isAuthorized } from "@/lib/middlewares/isAuthorized";
 
 export default withAuth({
@@ -33,6 +32,18 @@ export default withAuth({
   },
 });
 
+// We can't move this to /lib/auth until it supports spread operators
+// https://nextjs.org/docs/messages/invalid-page-config
 export const config = {
-  defaultConfig,
+  matcher: [
+    /*
+     * Match all paths except for:
+     * 1. / index page
+     * 2. /api routes
+     * 3. /_next (Next.js internals)
+     * 4. /_static (inside /public)
+     * 5. all root files inside /public (e.g. /favicon.ico)
+     */
+    "/((?!$|api/|_next/|_static/|[\\w-]+\\.\\w+).*)",
+  ],
 };
