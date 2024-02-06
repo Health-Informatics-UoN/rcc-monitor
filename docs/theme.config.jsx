28da58a00d4670d48718e3cdaa646f3a7de35ccc
Andy Rae <hello@rae.li>
2024-02-06 15:42:53 +0000

    Move dev docs

--- docs/theme.config.jsx
+++ docs/theme.config.jsx
@@ -1,7 +1,6 @@
 export default {
-  logo: <span>rcc-monitor Documentation</span>,
+  logo: <span>RCC-Monitor Documentation</span>,
   project: {
     link: "https://github.com/Health-Informatics-UoN/rcc-monitor",
   },
-  // ... other theme options
 };
