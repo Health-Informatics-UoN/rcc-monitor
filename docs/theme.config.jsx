6e2d05bfd87e5356e110a8c87b7c10bb31520cac
Andy Rae <hello@rae.li>
2024-02-06 15:16:51 +0000

    Add initial config

---
+++ docs/theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span>rcc-monitor Documentation</span>,
+  project: {
+    link: "https://github.com/Health-Informatics-UoN/rcc-monitor",
+  },
+  // ... other theme options
+};
