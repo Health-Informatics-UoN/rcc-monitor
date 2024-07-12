30a982d0b8b256deda06a0f6b832aed322508c32
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-07-12 14:43:51 +0100

    Update home components to tailwind

--- app/frontend-webapp/components/home/LandingPage.tsx
+++ app/frontend-webapp/components/home/LandingPage.tsx
@@ -1,26 +1,10 @@
-import { css } from "@/styled-system/css";
-
 export default function LandingPage() {
   return (
     <>
-      <h2
-        className={css({
-          m: "10px 10px 0px 10px",
-          p: "10px",
-          fontSize: "35px",
-          fontWeight: "500",
-        })}
-      >
+      <h2 className="mt-[10px] mr-[10px] mb-[0px] ml-[10px] p-[10px] text-[35px] font-medium">
         RedCap Monitor
       </h2>
-      <p
-        className={css({
-          m: "0px 10px",
-          p: "10px",
-          fontSize: "18px",
-          lineHeight: "50px",
-        })}
-      >
+      <p className="my-0 mx-[10px] p-[10px] text-lg leading-[50px]">
         A tool to monitor and enhance NUH RedCap Clinical Trials. <br />
         Part of the University of Nottingham Digital Research Service and
         Nottingham University Hospital NHS Trust collaboration.
