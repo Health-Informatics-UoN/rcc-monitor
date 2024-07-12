e4278be2fc622ec7f3c903bfe8e305c6b7369d29
Emeka Akashili <chukwuemeka.akashili@gmail.com>
2024-07-12 14:46:31 +0100

    Update synthetic data page to tailwind

--- app/frontend-webapp/app/synthetic-data/page.tsx
+++ app/frontend-webapp/app/synthetic-data/page.tsx
@@ -1,13 +1,7 @@
 import { Metadata } from "next";
-
-import { css } from "@/styled-system/css";
-import { Box, Flex } from "@/styled-system/jsx";
 import { AlertCircle } from "lucide-react";
-import { grid } from "@/styled-system/patterns";
-
-import { Alert, AlertTitle } from "@/components/shadow-ui/Alert";
 import { UploadFile } from "./form";
-import { h1 } from "@/styled-system/recipes";
+import { Alert, AlertTitle } from "@/components/ui/alert";
 
 export const metadata: Metadata = {
   title: "RedCap Synthetic Data",
@@ -15,15 +9,9 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   return (
-    <div
-      className={grid({
-        columns: { base: 1, md: 2 },
-        gap: "6",
-        lineHeight: "2rem",
-      })}
-    >
-      <Box>
-        <h1 className={h1()}>Synthetic Data Generation</h1>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6 leading-8">
+      <div>
+        <h1 className={`h1`}>Synthetic Data Generation</h1>
         <p>
           A tool that will generate synthetic data if you upload a RedCap Cloud
           data dictionary. This data is suitable for adding subjects to a study
@@ -33,11 +21,7 @@ export default async function Page() {
           The tool will generate data based on the type of field and the minimum
           and maximum validation criteria. It will not:
         </p>
-        <ol
-          className={css({
-            listStyle: "disc",
-          })}
-        >
+        <ol className="list-disc">
           <li>Test the data dictionary.</li>
           <li>Generate realistic synthetic data.</li>
           <li>Respect branching logic (this is handled by RedCap on import)</li>
@@ -47,21 +31,8 @@ export default async function Page() {
           <li>Understand fields beyond the field type.</li>
         </ol>
 
-        <h2
-          className={css({
-            fontSize: "1.5rem",
-            fontWeight: "bold",
-            m: "20px 0px",
-          })}
-        >
-          How to use it
-        </h2>
-        <ol
-          className={css({
-            listStyleType: "decimal",
-            listStyle: "decimal",
-          })}
-        >
+        <h2 className="text-2xl font-bold my-[20px] mx-0 ">How to use it</h2>
+        <ol className="list-decimal">
           <li>
             Download your RedCap data dictionary from RedCap cloud. Go to your
             study, the Instruments tab, the Tools dropdown, and select Export
@@ -93,17 +64,17 @@ export default async function Page() {
             The synthetic data should now have populated your subjects data.
           </li>
         </ol>
-      </Box>
+      </div>
 
-      <Flex direction={"column"} justifyContent={"center"} gap={"6"}>
+      <div className="flex flex-col justify-center gap-6">
         <UploadFile />
-        <Alert backgroundColor="blue.200">
-          <AlertCircle />
+        <Alert className="bg-blue-200">
+          <AlertCircle className="icon-lg" />
           <AlertTitle>
             You can upload a data dictionary in a supported format: .csv
           </AlertTitle>
         </Alert>
-      </Flex>
+      </div>
     </div>
   );
 }
